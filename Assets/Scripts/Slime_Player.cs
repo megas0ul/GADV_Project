3ba0a9a2e793a7a15ab1a2c486fb37260d22fc38
megas0ul <megasoulthegamer@gmail.com>
2026-06-30 17:07:17 +0800

    Added basic user inputs
    Slime is able to move up, left and right but need to tweak the speed and fix the infinite jump.

--- Assets/Scripts/Slime_Player.cs
+++ Assets/Scripts/Slime_Player.cs
@@ -2,22 +2,35 @@ using UnityEngine;
 
 public class Slime_Player : MonoBehaviour
 {
+    private Rigidbody2D _myRb;
+    private Vector2 _direction;
+    public float speed = 8f;
+    public float jump_speed = 3f;
+        
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-                _direction = Vector2.left;
-            } else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-                _direction = Vector2.right;
-            } else {
-                _direction = Vector2.zero;
-            }
-        
+        _myRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A)) {
+            _direction = Vector2.left;
+        } else if(Input.GetKey(KeyCode.D)) {
+            _direction = Vector2.right;
+        } else if(Input.GetKey(KeyCode.Space)) {  
+            _myRb.AddForce(transform.up * jump_speed);
+        } else {
+            _direction = Vector2.zero;
+        }
+    }
+    void FixedUpdate()
+    {
+        if(_direction == Vector2.zero) {
+            return;
+        }
+        _myRb.linearVelocity = new Vector2(_direction.x * speed, _myRb.linearVelocity.y);
     }
 }
