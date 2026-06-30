3f62342d3c3d545907abe630a80b50dc7a53b7fb
megas0ul <megasoulthegamer@gmail.com>
2026-06-30 14:20:13 +0800

    Added Sprites
    used sprite renderer to add the playable character into the game

--- Assets/Scripts/Slime_Player.cs
+++ Assets/Scripts/Slime_Player.cs
@@ -5,6 +5,13 @@ public class Slime_Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+                _direction = Vector2.left;
+            } else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+                _direction = Vector2.right;
+            } else {
+                _direction = Vector2.zero;
+            }
         
     }
 
